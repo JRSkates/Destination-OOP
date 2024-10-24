8d16df37c9723cef7aaf18f4aad9eed981d4464a
JRSkates <skatesproduction@gmail.com>
2024-10-24 15:25:34 +0100

    Person class test driven

--- __tests__/classes/Person.test.js
+++ __tests__/classes/Person.test.js
@@ -1,8 +1,21 @@
-const Person = require("../classes/Person.js");
+const Person = require("../../classes/Person.js");
+const Bag = require("../../classes/Bag.js");
 
 describe("Person Class", () => {
+    const person = new Person("Jack", "New York");
+    const bag = new Bag(45, 1);
     it("created a Person instance", () => {
-        
+        expect(person.name).toBe("Jack");
+        expect(person.destination).toBe("New York");
+    })
+
+    it("returns the bag property with getBags", () => {
+        expect(person.getBags()).toEqual([]);
+    })
+
+    it("adds to the bag property with addBag method", () => {
+        person.addBag(bag);
+        expect(person.getBags()).toEqual([bag]);
     })
 })
 
--- classes/Person.js
+++ classes/Person.js
@@ -1,8 +1,19 @@
 class Person {
-    constructor(name) {
+    #bags
+    constructor(name, destination) {
         this.name = name;
+        this.destination = destination;
+        this.#bags = [];
     }
 
+    getBags() {
+        return this.#bags;
+    }
+
+    addBag(bag) {
+        this.#bags.push(bag);
+        bag.assignOwner(this);
+    }
 }
 
 
